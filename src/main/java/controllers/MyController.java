5da77a613ce8f119351cb15607a0f210952e10ab
kirillmoiseenkov@mail.ru <kirillmoiseenkov@mail.ru>
2018-03-31 14:11:34 +0400

    Настроил среду для работы с RestController. Добавил Jackson и изменил версию Spring

---
+++ src/main/java/controllers/MyController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import models.main.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import service.UserService;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class MyController {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    ApplicationContext context = new ClassPathXmlApplicationContext("mainSpringConf.xml");
+
+    UserService userService = context.getBean(UserService.class);
+
+    @GetMapping(value = "/hello")
+    public User hello(ModelMap model){
+
+        User user = new User();
+        user.setId(1L);
+        user.setName("Kirill");
+        user.setLogin("login");
+        user.setPassword("password");
+        user.setLastName("moiseenkov");
+        //System.out.println("Pozor - hello");
+        System.out.println("something");
+
+        //model.addAttribute("message", "Spring 3 MVC Hello World!!!");
+        return userService.getById(1L);//"MyHello";
+
+    }
+
+}
